a70ad5345b7a731d7d1aafd77b1d5c984fd23763
Roberto Lopez <ing.robertolc@gmail.com>
2023-01-03 10:44:13 -0600

    Se agregan comentarios para describir los métodos

--- src/main/java/com/robertolc/util/DateLeapYearUtil.java
+++ src/main/java/com/robertolc/util/DateLeapYearUtil.java
@@ -1,7 +1,11 @@
 package com.robertolc.util;
 
 public class DateLeapYearUtil {
-
+    /**
+     * Descripción: Método que calcula si un año es bisiesto
+     * @param year
+     * @return
+     */
     public static boolean isLeapYear(int year){
         //Validamos que sea divisible por 4 pero no por 100
         //Validamos si el año es divisible por 400
